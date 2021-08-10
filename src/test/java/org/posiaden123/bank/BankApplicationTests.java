435a0e83376026fbe4ad025a588ae40e7f7985f1
posiaden123 <dewest88@gmail.com>
2021-08-09 22:13:22 -0400

    boilerplate project structure

---
+++ src/test/java/org/posiaden123/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package org.posiaden123.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
