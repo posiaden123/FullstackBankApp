8346a9322e86b8d3b25483b13cd13a381995791e
posiaden123 <dewest88@gmail.com>
2021-08-12 19:33:08 -0400

    boilerplate security config

---
+++ src/main/java/org/posiaden/bank/config/BankSecurityInitializer.java
@@ -0,0 +1,7 @@
+package org.posiaden.bank.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class BankSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
