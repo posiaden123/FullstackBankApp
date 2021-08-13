dcdc1c33c6539cab00242f5e62b15f6c98cdb04a
posiaden123 <dewest88@gmail.com>
2021-08-12 20:16:24 -0400

    login page setup - not the best at frontend so login panel is lopsided

--- src/main/java/org/posiaden/bank/config/BankSecurityConfig.java
+++ src/main/java/org/posiaden/bank/config/BankSecurityConfig.java
@@ -2,6 +2,7 @@ package org.posiaden.bank.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,5 +12,17 @@ public class BankSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/signin")
+                .loginProcessingUrl("/authenticate")
+                .permitAll();
     }
 }
